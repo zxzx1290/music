95b31b434e8acf8ecd704b95a79d0851c16b9fa4
zxzx1290 <indexzexx@gmail.com>
2018-07-10 19:29:03 +0800

    responsive view init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatSliderModule } from '@angular/material/slider';
 
 import { AppMainComponent } from './app-main/app-main.component';
@@ -36,7 +35,6 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     BrowserAnimationsModule,
 
-    FlexLayoutModule,
     MatSliderModule
   ],
   providers: [],
